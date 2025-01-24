048b4de9f4ae19489a8f04a28498d3f18dc63e4d
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-01-24 18:35:18 +0300

    api versioning has been create v1.0

--- Services/Catalog/MultiShop.Catalog/Controllers/v1/CategoryController.cs
+++ Services/Catalog/MultiShop.Catalog/Controllers/v1/CategoryController.cs
@@ -27,5 +27,36 @@ namespace MultiShop.Catalog.Controllers.v1
             var response=new ResponseDto<List<ResultCategoryDto>>(list, "Categories listed successfully", 200);
             return StatusCode(response.Status, response);
         }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ResponseDto<GetByIdCategoryDto>>> GetCategory(string id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            var response = new ResponseDto<GetByIdCategoryDto>(category, "Category got successfully", 200);
+            return StatusCode(response.Status, response);
+        }
+
+        [HttpPost("add")]
+        public async Task<ActionResult<ResponseDto>> AddCategory([FromBody] CreateCategoryDto createCategoryDto)
+        {
+            await _categoryService.CreateAsync(createCategoryDto);
+            var response = new ResponseDto("Category added successfully", 200);
+            return StatusCode(response.Status, response);
+        }
+
+        [HttpPut("update")]
+        public async Task<ActionResult<ResponseDto>> UpdateCategory([FromBody] UpdateCategoryDto updateCategoryDto)
+        {
+            await _categoryService.UpdateAsync(updateCategoryDto);
+            var response = new ResponseDto("Category updated successfully", 200);
+            return StatusCode(response.Status, response);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public  async Task<ActionResult<ResponseDto>> DeleteCategory(string id)
+        {
+            await _categoryService.DeleteAsync(id);
+            var response = new ResponseDto("Category deleted successfully", 200);
+            return StatusCode(response.Status, response);
+        }
     }
 }
