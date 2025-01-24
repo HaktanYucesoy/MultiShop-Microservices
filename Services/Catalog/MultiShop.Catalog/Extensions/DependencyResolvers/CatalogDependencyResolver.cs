6fb7e48588b107c1482840fb73133d247d18d5a3
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-01-24 18:36:38 +0300

    Created to dependency resolver

--- Services/Catalog/MultiShop.Catalog/Extensions/DependencyResolvers/CatalogDependencyResolver.cs
+++ Services/Catalog/MultiShop.Catalog/Extensions/DependencyResolvers/CatalogDependencyResolver.cs
@@ -1,4 +1,7 @@
-﻿using MultiShop.Catalog.Services.CategoryServices;
+﻿using MultiShop.Catalog.Aspects.ExceptionHandling;
+using MultiShop.Catalog.Aspects.Extensions;
+using MultiShop.Catalog.Aspects.Mappings;
+using MultiShop.Catalog.Services.CategoryServices;
 using MultiShop.Catalog.Services.ProductDetailServices;
 using MultiShop.Catalog.Services.ProductImageServices;
 using MultiShop.Catalog.Services.ProductServices;
@@ -10,10 +13,24 @@ namespace MultiShop.Catalog.Extensions.DependencyResolvers
     {
         public static IServiceCollection AddCatalogDependencies(this IServiceCollection services)
         {
-            services.AddScoped<ICategoryService, CategoryService>();
-            services.AddScoped<IProductService, ProductService>();
-            services.AddScoped<IProductDetailService, ProductDetailService>();
-            services.AddScoped<IProductImageService,ProductImageService>();
+
+            services.AddDomainService<CategoryDomain, ICategoryService, CategoryService>(
+                CategoryExceptionMapping.GetExceptionMap());
+
+            services.AddDomainService<ProductDomain, IProductService, ProductService>(
+                ProductExceptionMapping.GetExceptionMap());
+
+            services.AddDomainService<ProductDetailDomain, IProductDetailService, ProductDetailService>(
+                ProductDetailExceptionMapping.GetExceptionMap());
+
+            services.AddDomainService<ProductImageDomain, IProductImageService, ProductImageService>(ProductImageExceptionMapping.GetExceptionMap());
+
+            //services.AddScoped<ICategoryService, CategoryService>();
+            //services.AddScoped<IProductService, ProductService>();
+            //services.AddScoped<IProductImageService, ProductImageService>();
+            //services.AddScoped<IProductDetailService, ProductDetailService>();
+            //services.AddScoped<DomainExceptionRegistery>();
+
 
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
