0bf8a33ae75b52f693ca69dc81179e2394fc06aa
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-09 21:01:13 +0300

    Exception messages have been defined for discount exceptions

---
+++ Services/Discount/MultiShop.Discount/Constables/Messages/DiscountExceptionMessages.cs
@@ -0,0 +1,17 @@
+﻿namespace MultiShop.Discount.Constables.Messages
+{
+    public static class DiscountExceptionMessages
+    {
+        public const string NotDiscountFound = "No coupon found";
+
+        public const string ErrorOccuredWhileAddingDiscount = "An error occurred while adding a coupon";
+
+        public const string ErrorOccuredWhileUpdatingDiscount = "An error occurred while updating the coupon";
+
+        public const string ErrorOccuredWhileDeletingDiscount = "An error occurred while deleting the coupon";
+
+        public const string ErrorOccuredWhileGettingDiscounts = "An error occurred while getting coupons";
+
+        public const string ErrorOccuredWhileGettingDiscount = "An error occurred while getting coupon";
+    }
+}
