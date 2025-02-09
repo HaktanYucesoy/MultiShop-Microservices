53ac9ebff65936eb57820a8424eeb55056bb95b9
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-09 20:59:47 +0300

    Dtos have been created for Discount domain

---
+++ Services/Discount/MultiShop.Discount/Dtos/Coupon/DeleteCouponDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiShop.Discount.Dtos.Coupon
+{
+    public class DeleteCouponDto
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ Services/Discount/MultiShop.Discount/Dtos/Response/ResponseDto.cs
@@ -0,0 +1,6 @@
+﻿namespace MultiShop.Discount.Dtos.Response
+{
+    public record ResponseDto(string Message, int Status);
+
+    public record ResponseDto<T>(T data, string Message, int Status);
+}
