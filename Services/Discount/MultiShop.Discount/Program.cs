d7d0259290f7e8286041ae9eaa36fa36436d29d0
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-10 15:23:04 +0300

    Some packages have been installed for this discount project and also settings have been made it from Program.cs

--- Services/Discount/MultiShop.Discount/Program.cs
+++ Services/Discount/MultiShop.Discount/Program.cs
@@ -1,13 +1,67 @@
+
+
+using Asp.Versioning;
+using MultiShop.Discount.Extensions.DependencyResolvers;
+using MultiShop.Discount.Handlers.Exception;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Serilog yapýlandýrmasý
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .Enrich.WithMachineName()
+    .Enrich.WithThreadId()
+    .Enrich.WithEnvironmentName()
+    .WriteTo.Console(new JsonFormatter())
+    .WriteTo.File(new JsonFormatter(),
+        path: "logs/discount-service-.log",
+        rollingInterval: RollingInterval.Day,
+        retainedFileCountLimit: 30)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDiscountDependencies();
+builder.Services.AddApiVersioning(x =>
+{
+    x.AssumeDefaultVersionWhenUnspecified = true;
+    x.DefaultApiVersion = ApiVersion.Default;
+    x.ReportApiVersions = true;
+    x.ApiVersionReader = ApiVersionReader.Combine(
+        new QueryStringApiVersionReader("api-version"),
+        new HeaderApiVersionReader("api-version"),
+        new UrlSegmentApiVersionReader()
+        );
+}).AddApiExplorer(x =>
+{
+    x.GroupNameFormat = "'v'V";
+    x.SubstituteApiVersionInUrl = true;
+});
+builder.Services.AddProblemDetails();
+builder.Services.AddExceptionHandler<CustomDiscountExceptionHandler>();
 
 var app = builder.Build();
+
+// Exception handling middleware
+app.UseSerilogRequestLogging(options =>
+{
+    options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+    {
+        diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+        diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
+        diagnosticContext.Set("ClientIP", httpContext.Connection.RemoteIpAddress!);
+    };
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,10 +70,23 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+app.UseExceptionHandler();
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
-app.Run();
+try
+{
+    Log.Information("Starting Discount Service");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
