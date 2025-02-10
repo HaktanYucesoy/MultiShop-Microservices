8bc49eafeb021c33a50958fe928b48a24ead7511
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-10 15:22:24 +0300

    DI settings have been handled for discount

---
+++ Services/Discount/MultiShop.Discount/Extensions/DependencyResolvers/DiscountDependencyExtension.cs
@@ -0,0 +1,21 @@
+﻿using MultiShop.Discount.Context;
+using MultiShop.Discount.Services;
+using MultiShop.Discount.Services.Decorators;
+
+namespace MultiShop.Discount.Extensions.DependencyResolvers
+{
+    public static class DiscountDependencyExtension
+    {
+        public static IServiceCollection AddDiscountDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<DapperContext>();
+            services.AddScoped<IDiscountService, DiscountService>();
+            services.Decorate<IDiscountService>((inner, provider) => new LoggingDiscountDecorator(
+             inner,
+             provider.GetRequiredService<ILogger<LoggingDiscountDecorator>>()
+         ));
+
+            return services;
+        }
+    }
+}
