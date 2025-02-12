0cc7cb1017bebd9d1cf98be9484f09a0d5300d1b
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-12 15:56:34 +0300

    Base Repository has been created

---
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Linq.Expressions;
+
+namespace MultiShop.Order.Application.Interfaces
+{
+    public interface IRepository<T,TId> where T:class
+
+    {
+        Task<List<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(TId id);
+
+        Task<T> AddAsync(T entity);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task<bool> DeleteAsync(TId id);
+
+        Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter);
+    }
+}
