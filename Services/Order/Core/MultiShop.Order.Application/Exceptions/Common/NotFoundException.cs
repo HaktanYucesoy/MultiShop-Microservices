77e618435261479e0c3f3f8d6965e1a1010fc1e1
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-16 12:20:58 +0300

    First common exception has been created for not found issue

---
+++ Services/Order/Core/MultiShop.Order.Application/Exceptions/Common/NotFoundException.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MultiShop.Order.Application.Exceptions.Common
+{
+    public class NotFoundException:Exception
+    {
+
+        public NotFoundException(string name, object key)
+            : base($"{name} with ({key}) was not found.")
+        {
+
+        }
+       
+        public NotFoundException(string extendedMessage)
+            : base(extendedMessage)
+        {
+        }
+    }
+}
