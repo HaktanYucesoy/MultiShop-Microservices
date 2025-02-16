b96234528ecea2f18c14b11dc69c9adc73d01021
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-16 12:19:43 +0300

    Custom Db functions have been defined for Ordering and related entities

--- Services/Order/Core/MultiShop.Order.Application/Interfaces/Repositories/IOrderingRepository.cs
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/Repositories/IOrderingRepository.cs
@@ -4,5 +4,11 @@ namespace MultiShop.Order.Application.Interfaces.Repositories
 {
     public interface IOrderingRepository:IRepository<Ordering,int>
     {
+        Task<Ordering> UpdateOrderWithAddedToNewOrderDetail(Ordering ordering, OrderDetail orderDetail);
+
+        Task<Ordering> UpdateOrderWithUpdatedToNewOrderDetailsAndAddress(Ordering ordering, List<OrderDetail> orderDetails, Address deliveryAddress);
+        Task<Ordering> GetOrderingWithOrderDetails(int id);
+
+        Task<Ordering> GetOrderingWithOrderDetailsAndAddress(int id);
     }
 }
