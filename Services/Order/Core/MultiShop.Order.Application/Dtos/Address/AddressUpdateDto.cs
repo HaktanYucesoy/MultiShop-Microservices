9a19aff75dd3d5e2173417d017eb20b5b69e86b4
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-16 12:20:16 +0300

    Dtos have been defined for using property to UpdateOrderingCommandRequest class

---
+++ Services/Order/Core/MultiShop.Order.Application/Dtos/Address/AddressUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MultiShop.Order.Application.Dtos.Address
+{
+    public class AddressUpdateDto
+    {
+       
+        public string District { get; set; }
+        public string City { get; set; }
+        public string Detail { get; set; }
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Dtos/OrderDetail/OrderDetailDto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MultiShop.Order.Application.Dtos.OrderDetail
+{
+    public class OrderDetailDto
+    {
+        public int OrderDetailId { get; set; }
+        public int NewAmount { get; set; }
+        public decimal NewPrice { get; set; }
+        public string NewName { get; set; }
+
+        public string NewImage { get; set; }
+    }
+}
