6846eb06d30b1a95ff37bfc32568ba2338086407
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-16 12:22:26 +0300

    Application's Service registration have been created

---
+++ Services/Order/Core/MultiShop.Order.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+
+namespace MultiShop.Order.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection service,IConfiguration configuration)
+        {
+            var assembly=Assembly.GetExecutingAssembly();
+            service.AddMediatR(cfg=>cfg.RegisterServicesFromAssembly(assembly));
+            service.AddAutoMapper(assembly);
+            return service;
+
+        }
+    }
+}
