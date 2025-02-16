af2f2589caee233e9e5770bc56cba7e1cc135d13
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-16 12:17:43 +0300

    First MediatR Pipeline implemantion has been made it for Transaction

---
+++ Services/Order/Core/MultiShop.Order.Application/Behaviors/TransactionBehavior.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using MultiShop.Order.Application.Interfaces.Transaction;
+using MultiShop.Order.Application.Interfaces.UnitOfWork;
+
+namespace MultiShop.Order.Application.Behaviors
+{
+    public class TransactionBehavior<TRequest, TResponse> :
+        IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TransactionBehavior(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (!(request is ITransactionalCommand))
+                return await next();
+
+            using var transaction = await _unitOfWork.BeginTransactionAsync(cancellationToken);
+            try
+            {
+                var response = await next();
+                await transaction.CommitAsync(cancellationToken);
+                return response;
+            }
+            catch
+            {
+                await transaction.RollbackAsync(cancellationToken);
+                throw;
+            }
+        }
+    }
+}
