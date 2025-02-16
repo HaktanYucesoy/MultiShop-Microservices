f16f3ea44888f84f30e8cb7a1917ebff04998df9
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-16 12:19:11 +0300

    UnitOfWork interface has been created but UnitOfWork implemantation should make it for Ef Core

---
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using MultiShop.Order.Application.Interfaces.Transaction;
+
+namespace MultiShop.Order.Application.Interfaces.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task<ITransaction> BeginTransactionAsync(CancellationToken cancellationToken = default);
+        Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+        Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
+    }
+}
