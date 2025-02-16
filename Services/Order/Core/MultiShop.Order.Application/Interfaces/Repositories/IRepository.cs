009564366eb2b068b7ae3d348af93765dd186a79
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-16 12:20:35 +0300

    New get list and get functions have been created for generically

--- Services/Order/Core/MultiShop.Order.Application/Interfaces/Repositories/IRepository.cs
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/Repositories/IRepository.cs
@@ -1,20 +1,23 @@
-﻿using System.Linq.Expressions;
+﻿using MultiShop.Order.Domain.Common;
+using System.Linq.Expressions;
 
 namespace MultiShop.Order.Application.Interfaces.Repositories
 {
-    public interface IRepository<T, TId> where T : class
-
+    public interface IRepository<T, TId> where T : BaseEntity<TId>
     {
-        Task<List<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAllWithIncludesAsync(params Expression<Func<T, object>>[] includes);
+        Task<IReadOnlyList<T>> GetListByFilterAsync(Expression<Func<T, bool>> filter,
+            params Expression<Func<T, object>>[] includes);
+
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> UpdateWithNavigationAsync(T entity, params Expression<Func<T, object>>[] includeProperties);
+        Task<bool> DeleteAsync(TId id);
 
         Task<T> GetByIdAsync(TId id);
 
-        Task<T> AddAsync(T entity);
-
-        Task<T> UpdateAsync(T entity);
-
-        Task<bool> DeleteAsync(TId id);
-
+        Task<T> GetByIdWithIncludesAsync(TId id, params Expression<Func<T, object>>[] includes);
         Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter);
     }
 }
