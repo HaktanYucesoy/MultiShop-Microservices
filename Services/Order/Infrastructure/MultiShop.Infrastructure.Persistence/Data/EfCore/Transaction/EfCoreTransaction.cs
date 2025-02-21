ba6710a779a5eec413a8c35e8f9261f09686c8f0
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-21 12:41:18 +0300

    Transaction function implemantations have been made it for Entity Framework Core

---
+++ Services/Order/Infrastructure/MultiShop.Infrastructure.Persistence/Data/EfCore/Transaction/EfCoreTransaction.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using MultiShop.Order.Application.Interfaces.Transaction;
+
+
+namespace MultiShop.Order.Infrastructure.Persistence.Data.EfCore.Transaction
+{
+    public class EfCoreTransaction : ITransaction
+    {
+        private readonly IDbContextTransaction _transaction;
+
+        public EfCoreTransaction(IDbContextTransaction transaction)
+        {
+            _transaction = transaction;
+        }
+
+        public async Task CommitAsync(CancellationToken cancellationToken = default)
+        {
+            await _transaction.CommitAsync(cancellationToken);
+        }
+
+        public void Dispose()
+        {
+            _transaction.Dispose();
+        }
+
+        public async Task RollbackAsync(CancellationToken cancellationToken = default)
+        {
+            await _transaction.RollbackAsync(cancellationToken);
+        }
+    }
+}
