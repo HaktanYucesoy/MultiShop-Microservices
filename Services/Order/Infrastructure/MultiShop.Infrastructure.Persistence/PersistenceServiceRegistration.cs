f39da63ffbb004e600a344375b3d756367ebb1cc
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-21 12:44:31 +0300

    Transaction and UnitOfWork DI has been made it to service registration

--- Services/Order/Infrastructure/MultiShop.Infrastructure.Persistence/PersistenceServiceRegistration.cs
+++ Services/Order/Infrastructure/MultiShop.Infrastructure.Persistence/PersistenceServiceRegistration.cs
@@ -1,15 +1,26 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using MultiShop.Order.Application.Interfaces.Repositories;
+using MultiShop.Order.Application.Interfaces.Transaction;
+using MultiShop.Order.Application.Interfaces.UnitOfWork;
 using MultiShop.Order.Infrastructure.Persistence.Data.EfCore.Context;
 using MultiShop.Order.Infrastructure.Persistence.Data.EfCore.Repositories;
+using MultiShop.Order.Infrastructure.Persistence.Data.EfCore.Transaction;
+using MultiShop.Order.Infrastructure.Persistence.Data.EfCore.UnitOfWork;
 
 namespace MultiShop.Order.Infrastructure.Persistence
 {
     public static class PersistenceServiceRegistration
     {
-        public static IServiceCollection AddPersistenceService(this IServiceCollection services)
+        public static IServiceCollection AddPersistenceService(this IServiceCollection services,IConfiguration configuration)
         {
-            services.AddDbContext<OrderContext>();
+            services.AddDbContext<OrderContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });         
+            services.AddScoped<ITransaction, EfCoreTransaction>();
+            services.AddScoped<IUnitOfWork, EfCoreUnitOfWork>();
             services.AddScoped<IOrderingRepository, EfCoreOrderingRepository>();
             services.AddScoped<IOrderDetailRepository, EfCoreOrderDetailRepository>();
             services.AddScoped<IAddressRepository, EfCoreAddressRepository>();
