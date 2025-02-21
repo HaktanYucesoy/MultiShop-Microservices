d6dd983bc85d708d827f6fc40e8bd51aed1392ef
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-02-21 12:36:55 +0300

    New common  exception classes have been created

---
+++ Services/Order/Core/MultiShop.Order.Application/Exceptions/Common/DeleteFailureException.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MultiShop.Order.Application.Exceptions.Common
+{
+    public class DeleteFailureException:Exception
+    {
+        public string DomainName { get; set; }
+        public int Id { get; set; }
+        public string ExtendedMessage { get; set; }
+        public DeleteFailureException(string domainName, int id, string extendedMessage)
+            : base($"Failed to delete {domainName} with id {id}. {extendedMessage}")
+        {
+            domainName = DomainName;
+            Id = id;
+            ExtendedMessage = extendedMessage;
+        }
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Exceptions/Common/UpdateFailureException.cs
@@ -0,0 +1,19 @@
+﻿
+namespace MultiShop.Order.Application.Exceptions.Common
+{
+    public class UpdateFailureException:Exception
+    {
+        public string DomainName { get; set; }
+        public int Id { get; set; }
+        public string ExtendedMessage { get; set; }
+        public UpdateFailureException(string domainName, int id, string extendedMessage)
+            :base($"Failed to update {domainName} with id {id}. {extendedMessage}")
+        {
+            domainName = DomainName;
+            Id = id;
+            ExtendedMessage = extendedMessage;
+        }
+
+       
+    }
+}
