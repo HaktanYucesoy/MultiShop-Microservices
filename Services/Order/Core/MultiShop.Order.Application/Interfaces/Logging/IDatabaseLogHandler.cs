7d0e7a65103f068d883e87a57fbda64d8d462641
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-03-05 23:23:44 +0300

    Interface segeration has been made it for Database log handler

---
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/Logging/IDatabaseLogHandler.cs
@@ -0,0 +1,11 @@
+﻿using MultiShop.Order.Application.Enums;
+using MultiShop.Order.Application.Models.Logging;
+
+namespace MultiShop.Order.Application.Interfaces.Logging
+{
+    public interface IDatabaseLogHandler:ILogHandler
+    {
+        Task<IEnumerable<LogDetail>> QueryLogsAsync(DateTime startDate, DateTime endDate, LogLevel? level = null);
+        Task PurgeLogsAsync(DateTime olderThan);
+    }
+}
