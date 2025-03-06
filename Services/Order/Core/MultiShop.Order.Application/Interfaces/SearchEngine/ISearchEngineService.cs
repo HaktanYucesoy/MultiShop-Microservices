eb0f7dca61340deed1a6cd320dcf618668865020
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-03-06 13:33:00 +0300

    General search engine implemantion has been made it

---
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/SearchEngine/ISearchEngineService.cs
@@ -0,0 +1,14 @@
+﻿
+namespace MultiShop.Order.Application.Interfaces.SearchEngine
+{
+    public interface ISearchEngineService<T,TKey>
+    {
+        Task<bool> AddOrUpdate(T entity);
+        Task<T> Get(TKey key);
+
+        Task<IList<T>> GetAll();
+        Task<bool> Delete(TKey key);
+        Task<long?> DeleteAll();
+
+    }
+}
