f4fb9017de0b738e927dd75704c2144ae5bd08bc
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-03-06 13:33:23 +0300

    Pure ElasticSearch log handler has been created

---
+++ Services/Order/Infrastructure/MultiShop.Infrastructure/Logging/ElasticSearch/ElasticSearchLogHandler.cs
@@ -0,0 +1,22 @@
+﻿using MultiShop.Order.Application.Interfaces.Logging;
+using MultiShop.Order.Application.Models.Logging;
+using MultiShop.Order.Infrastructure.SearchEngine.ElasticSearch.Interfaces;
+
+namespace MultiShop.Order.Infrastructure.Logging.ElasticSearch
+{
+    public class ElasticSearchLogHandler : ILogHandler
+    {
+        private ILogDetailElasticSearchEngineService _logDetailElasticSearchEngineService;
+
+        public ElasticSearchLogHandler(ILogDetailElasticSearchEngineService logDetailElasticSearchEngineService)
+        {
+            _logDetailElasticSearchEngineService = logDetailElasticSearchEngineService;
+        }
+
+        public async Task HandleLogAsync(LogDetail logdetail)
+        {
+            await _logDetailElasticSearchEngineService.CreateIndexIfNotExistsAsync("order-logs");
+            await _logDetailElasticSearchEngineService.AddOrUpdate(logdetail);
+        }
+    }
+}
