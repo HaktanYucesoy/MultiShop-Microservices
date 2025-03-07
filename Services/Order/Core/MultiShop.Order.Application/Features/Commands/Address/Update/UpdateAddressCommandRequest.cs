651a702e90df51ece56b2ed674fc9d90ec81c14f
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-03-07 17:28:26 +0300

    Constructor method have been removed from two update command request classes

--- Services/Order/Core/MultiShop.Order.Application/Features/Commands/Address/Update/UpdateAddressCommandRequest.cs
+++ Services/Order/Core/MultiShop.Order.Application/Features/Commands/Address/Update/UpdateAddressCommandRequest.cs
@@ -6,18 +6,10 @@ namespace MultiShop.Order.Application.Features.Commands.Address.Update
 {
     public class UpdateAddressCommandRequest:IRequest<UpdateAddressCommandResponse>
     {
-        public UpdateAddressCommandRequest(int id, string city, string district, string userId)
-        {
-            Id = id;
-            City = city;
-            District = district;
-            UserId = userId;
-        }
-
         public int Id { get; set; }     
         public string City { get; set; }
         public string District { get; set; }
         public string UserId { get; set; }
-        public string Detail { get; internal set; }
+        public string Detail { get; set; }
     }
 }
--- Services/Order/Core/MultiShop.Order.Application/Features/Commands/OrderDetail/Update/UpdateOrderDetailCommandRequest.cs
+++ Services/Order/Core/MultiShop.Order.Application/Features/Commands/OrderDetail/Update/UpdateOrderDetailCommandRequest.cs
@@ -4,15 +4,6 @@ namespace MultiShop.Order.Application.Features.Commands.OrderDetail.Update
 {
     public class UpdateOrderDetailCommandRequest : IRequest<UpdateOrderDetailCommandResponse>
     {
-        public UpdateOrderDetailCommandRequest(int id, string productId, int productAmount, decimal productPrice, string productName, string productImage, int orderingId)
-        {
-            Id = id;
-            ProductId = productId;
-            ProductAmount = productAmount;
-            ProductPrice = productPrice;
-            ProductName = productName;
-            ProductImage = productImage;
-        }
         public int Id { get; set; }
         public string ProductId { get; set; }
         public int ProductAmount { get; set; }
