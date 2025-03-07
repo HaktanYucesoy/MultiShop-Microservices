aa694be7769ffe1b147638160236bbea91e2178c
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-03-07 17:29:21 +0300

    Custom Validation exception class has been created for when i can not passed to validation rules, Validation behaviour's Handle function will throw to this exception

---
+++ Services/Order/Core/MultiShop.Order.Application/Exceptions/Validation/CustomValidationException.cs
@@ -0,0 +1,20 @@
+﻿namespace MultiShop.Order.Application.Exceptions.Validation
+{
+    public class CustomValidationException:Exception
+    {
+
+        public CustomValidationException(IReadOnlyCollection<ValidationError> errors):
+            base("Validation fail")
+        {
+
+        }
+
+        public CustomValidationException(ValidationError error):base("Validation Fail")
+        {
+
+        }
+    }
+
+
+    public record ValidationError(string ErrorMessage,string PropertyName) { }
+}
