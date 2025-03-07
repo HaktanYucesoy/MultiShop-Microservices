7f05f39a044b137239014a6b1f51959a568c8f25
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-03-07 17:31:18 +0300

    MediatR's Validation Behavior class has been created for seperated validation codes to actual Command or Request handler functions(Cross Cutting Cornces)

---
+++ Services/Order/Core/MultiShop.Order.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using MediatR;
+using MultiShop.Order.Application.Exceptions.Validation;
+
+namespace MultiShop.Order.Application.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var context=new ValidationContext<TRequest>(request);
+            var errors= _validators
+                .Select(v => v.Validate(context))
+                .Where(v=>!v.IsValid)
+                .SelectMany(result => result.Errors)
+                .Select(validationFailure=>new ValidationError(validationFailure.PropertyName,validationFailure.ErrorMessage)).ToList();
+
+            if (errors.Any())
+            {
+                if (errors.Count == 1)
+                {
+                    throw new CustomValidationException(errors.FirstOrDefault()!);
+                }
+
+                else
+                {
+                    throw new CustomValidationException(errors);
+                }
+               
+            }
+
+            var response = await next();
+            return response;
+        }
+    }
+}
