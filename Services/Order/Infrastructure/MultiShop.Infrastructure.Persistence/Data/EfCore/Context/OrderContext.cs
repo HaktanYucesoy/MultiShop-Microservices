c4278a7df69315877aebf481452570d5f7e9d68c
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-03-10 13:48:48 +0300

    constructor method has been fixed with DbContextOptions parameter

--- Services/Order/Infrastructure/MultiShop.Infrastructure.Persistence/Data/EfCore/Context/OrderContext.cs
+++ Services/Order/Infrastructure/MultiShop.Infrastructure.Persistence/Data/EfCore/Context/OrderContext.cs
@@ -1,18 +1,25 @@
-﻿
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using MultiShop.Order.Domain.Entities;
 
 namespace MultiShop.Order.Infrastructure.Persistence.Data.EfCore.Context
 {
-    public class OrderContext:DbContext
+    public class OrderContext : DbContext
     {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=YourServer;Database=MultiShopOrderDb;Trusted_Connection=True;TrustServerCertificate=True");
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=HAKTANPC;Database=MultiShopOrderDb;Trusted_Connection=True;TrustServerCertificate=True");
+            }
         }
 
         public DbSet<Address> Addresses { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
         public DbSet<Ordering> Orderings { get; set; }
     }
+
 }
