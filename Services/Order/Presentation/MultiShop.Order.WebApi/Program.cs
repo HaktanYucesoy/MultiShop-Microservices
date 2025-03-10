bbdb5e8ca1bffe33c02c4ac214f637e5f8824037
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-03-10 13:51:01 +0300

    ServiceRegistrations have been added to Program.cs

--- Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,3 +1,7 @@
+using MultiShop.Order.Application;
+using MultiShop.Order.Infrastructure;
+using MultiShop.Order.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices();
+builder.Services.AddLoggingConfiguration(builder.Configuration);
+builder.Services.AddPersistenceService(builder.Configuration);
+
 
 var app = builder.Build();
 
