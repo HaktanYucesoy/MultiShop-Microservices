af63c3e99357ab48be21bb1d863afa473128e20c
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-03-12 15:55:38 +0300

    OrderDetailController's other CRUD Api endpoints have been created

---
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Controllers/v1/OrderDetailController.cs
@@ -0,0 +1,69 @@
+﻿using Asp.Versioning;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.Commands.OrderDetail.Create;
+using MultiShop.Order.Application.Features.Commands.OrderDetail.Delete;
+using MultiShop.Order.Application.Features.Commands.OrderDetail.Update;
+using MultiShop.Order.Application.Features.Queries.OrderDetail.GetById;
+using MultiShop.Order.Application.Features.Queries.OrderDetail.GetByOrderingId;
+
+namespace MultiShop.Order.WebApi.Controllers.v1
+{
+    [ApiController]
+    [ApiVersion(1)]
+    [Route("api/v{version:ApiVersion}/orderDetails")]
+    public class OrderDetailController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public OrderDetailController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddOrderDetailAsync(CreateOrderDetailCommandRequest request)
+        {
+
+            var response=await _mediator.Send(request);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrderDetailAsync(UpdateOrderDetailCommandRequest request)
+        {
+            var response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrderDetailAsync(int id)
+        {
+            var request=new DeleteOrderDetailCommandRequest(id);
+            var response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
+        [HttpGet("detailList/{orderingId}")]
+        public async Task<IActionResult> GetOrderDetailsOrderingId(int orderingId)
+        {
+            var request = new GetByOrderingIdOrderDetailQueryRequest(orderingId);
+
+            var response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderDetailById(int id)
+        {
+            var request = new GetByIdOrderDetailQueryRequest(id);
+            var response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
+
+    }
+
+}
