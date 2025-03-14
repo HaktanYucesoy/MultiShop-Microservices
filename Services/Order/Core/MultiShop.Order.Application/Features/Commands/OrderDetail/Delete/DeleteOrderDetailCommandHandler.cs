388b9d53864bad12d0607f32a175a63cafe8b389
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-03-14 23:29:43 +0300

    Delete order detail command handler has been edited

--- Services/Order/Core/MultiShop.Order.Application/Features/Commands/OrderDetail/Delete/DeleteOrderDetailCommandHandler.cs
+++ Services/Order/Core/MultiShop.Order.Application/Features/Commands/OrderDetail/Delete/DeleteOrderDetailCommandHandler.cs
@@ -6,19 +6,46 @@ namespace MultiShop.Order.Application.Features.Commands.OrderDetail.Delete
     public class DeleteOrderDetailCommandHandler : IRequestHandler<DeleteOrderDetailCommandRequest, DeleteOrderDetailCommandResponse>
     {
         private readonly IOrderDetailRepository _orderDetailRepository;
+        private readonly IOrderingRepository _orderingRepository;
 
-        public DeleteOrderDetailCommandHandler(IOrderDetailRepository orderDetailRepository)
+        public DeleteOrderDetailCommandHandler(IOrderDetailRepository orderDetailRepository, IOrderingRepository orderingRepository)
         {
             _orderDetailRepository = orderDetailRepository;
+            _orderingRepository = orderingRepository;
         }
         public async Task<DeleteOrderDetailCommandResponse> Handle(DeleteOrderDetailCommandRequest request, CancellationToken cancellationToken)
         {
             
-            var deleteResponse=await _orderDetailRepository.DeleteAsync(request.Id);
+            var existOrderDetail=await _orderDetailRepository.GetByIdAsync(request.Id);
+            if (existOrderDetail == null)
+            {
+                return new DeleteOrderDetailCommandResponse()
+                {
+                    IsSuccess = false
+                };
+            }
+
+            var existOrdering=await _orderingRepository.GetByIdWithIncludesAsync(existOrderDetail.OrderingId,x=>x.OrderDetails);
+
+            if(existOrdering==null)
+            {
+                return new DeleteOrderDetailCommandResponse()
+                {
+                    IsSuccess = false
+                };
+            }
+
+            existOrdering.DeleteOrderDetail(existOrderDetail.Id);
+
+            var response=await _orderingRepository.UpdateOrderByDeleteOrderDetail(existOrderDetail, existOrdering);
+
             return new DeleteOrderDetailCommandResponse()
             {
-                IsSuccess = deleteResponse
+                IsSuccess = response
             };
+
+
+
         }
     }
 }
