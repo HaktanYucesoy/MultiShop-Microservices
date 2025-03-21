34925b329ee9baa692ce468abaf9dccb62ea849d
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-03-21 16:05:40 +0300

    OtpNet implemantation has been made it for create one time password

---
+++ Services/Identity/Infrastructure/MultiShop.Identity.Infrastructure/OtpAuthenticator/OtpNet/OtpNetOtpAuthenticatorHelper.cs
@@ -0,0 +1,32 @@
+﻿using MultiShop.Identity.Application.Interfaces.OtpAuthenticator;
+using OtpNet;
+
+namespace MultiShop.Identity.Infrastructure.OtpAuthenticator.OtpNet
+{
+    public class OtpNetOtpAuthenticatorHelper : IOtpAuthenticatorHelper
+    {
+        public Task<string> ConvertSecretKeyToString(byte[] secretKey)
+        {
+           string base32String=Base32Encoding.ToString(secretKey);
+           return Task.FromResult(base32String);
+        }
+
+        public Task<byte[]> GenerateSecretKey()
+        {
+            byte[] key = KeyGeneration.GenerateRandomKey(20);
+            string base32String = Base32Encoding.ToString(key);
+            byte[] base32Bytes = Base32Encoding.ToBytes(base32String);
+
+            return Task.FromResult(base32Bytes);
+        }
+
+        public Task<bool> VerifyCode(byte[] secretKey, string code)
+        {
+            Totp toptp = new(secretKey);
+            string toptpCode = toptp.ComputeTotp();
+            bool result = toptpCode==code;
+
+            return Task.FromResult(result);
+        }
+    }
+}
