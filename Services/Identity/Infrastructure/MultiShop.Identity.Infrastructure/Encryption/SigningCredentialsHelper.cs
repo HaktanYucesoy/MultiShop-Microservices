f27425704d7d7604e03232c9b7e157c32bdc858a
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-03-21 16:06:53 +0300

    Encryption implemantations made it for create JWT token

---
+++ Services/Identity/Infrastructure/MultiShop.Identity.Infrastructure/Encryption/SigningCredentialsHelper.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace MultiShop.Identity.Infrastructure.Encryption
+{
+    public static class SigningCredentialsHelper
+    {
+        public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey) => new(securityKey, SecurityAlgorithms.HmacSha512Signature);
+    }
+}
