b2328d0a6fadf6f5c095cf3310645ebd099100a0
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-03-21 16:04:13 +0300

    Model or value objects have been created for used to code blocks but not save to db

---
+++ Services/Identity/Core/MultiShop.Identity.Domain/ValueObjects/AccessToken.cs
@@ -0,0 +1,21 @@
+﻿
+namespace MultiShop.Identity.Domain.ValueObjects
+{
+    public class AccessToken
+    {
+        public string Token { get; set; }
+
+        public DateTime Expiration {  get; set; }
+
+        public AccessToken()
+        {
+            Token=String.Empty;
+        }
+
+        public AccessToken(string token, DateTime expiration)
+        {
+            Token = token;
+            Expiration = expiration;
+        }
+    }
+}
---
+++ Services/Identity/Core/MultiShop.Identity.Domain/ValueObjects/TokenOptions.cs
@@ -0,0 +1,34 @@
+﻿
+namespace MultiShop.Identity.Domain.ValueObjects
+{
+    public class TokenOptions
+    {
+
+        public TokenOptions()
+        {
+            Auidence=String.Empty;
+            Issuer=String.Empty;
+            SecurityKey=String.Empty;
+        }
+        public TokenOptions(string auidence, string issuer, int accessTokenExpiration, string securityKey, int refreshTokenTTL)
+        {
+            Auidence = auidence;
+            Issuer = issuer;
+            AccessTokenExpiration = accessTokenExpiration;
+            SecurityKey = securityKey;
+            RefreshTokenTTL = refreshTokenTTL;
+        }
+
+        public string Auidence { get; set; }
+
+        public string Issuer { get; set; }
+
+        public int AccessTokenExpiration { get; set; }
+
+        public string SecurityKey { get; set; }
+
+        public int RefreshTokenTTL { get; set; }
+
+
+    }
+}
