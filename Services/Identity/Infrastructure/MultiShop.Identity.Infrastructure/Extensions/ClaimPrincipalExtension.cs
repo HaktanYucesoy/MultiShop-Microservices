0b34c720f228d005dcb777d22b130807438c019b
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-03-21 16:07:26 +0300

    User and user's claims get info extensions have been created for get extra info from logged in user

---
+++ Services/Identity/Infrastructure/MultiShop.Identity.Infrastructure/Extensions/ClaimPrincipalExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+
+namespace MultiShop.Identity.Infrastructure.Extensions
+{
+    public static class ClaimPrincipalExtension
+    {
+        public static List<string>? Claims(this ClaimsPrincipal principal,string claimType)
+        {
+            var result=principal.FindAll(claimType).Select(x=>x.Value).ToList();
+            return result;
+        }
+
+        public static List<string>? ClaimRoles(this ClaimsPrincipal principal) =>
+            principal.Claims(ClaimTypes.Role);
+
+
+        public static int GetUserId(this ClaimsPrincipal principal) =>
+            Convert.ToInt32(principal.Claims(ClaimTypes.NameIdentifier)!.FirstOrDefault());
+    }
+}
