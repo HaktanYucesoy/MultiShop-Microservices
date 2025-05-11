1ad2a1ecb7d1884a5d0c93f80b5641d9c813824e
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-05-11 19:27:45 +0300

    IdentiyContext has been created for created db

---
+++ Services/Identity/Infrastructure/MultiShop.Identity.Infrastructure.Persistence/EfCore/MultiShopIdentityContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MultiShop.Identity.Domain.Entities;
+using MultiShop.Identity.Infrastructure.Persistence.EfCore.Configurations;
+
+namespace MultiShop.Identity.Infrastructure.Persistence.EfCore
+{
+    public class MultiShopIdentityContext:DbContext
+    {
+        protected IConfiguration _configuration { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<OtpAuthenticator> OtpAuthenticators { get; set; }
+
+        public DbSet<EmailAuthenticator> EmailAuthenticators { get; set; }
+        public MultiShopIdentityContext(DbContextOptions<MultiShopIdentityContext> dbContextOptions,IConfiguration configuration):base(dbContextOptions)
+        {
+            _configuration=configuration;
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(UserConfiguration).Assembly);
+        }
+
+
+
+    }
+}
