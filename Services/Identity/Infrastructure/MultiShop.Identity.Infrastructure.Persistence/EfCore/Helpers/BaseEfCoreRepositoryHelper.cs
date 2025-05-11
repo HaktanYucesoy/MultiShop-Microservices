313ff2dfc583fb2a1d6533d46d30f08312e22875
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-05-11 19:27:28 +0300

    RepositoryHelper has been created for  manage to execute action and throw exception situation

---
+++ Services/Identity/Infrastructure/MultiShop.Identity.Infrastructure.Persistence/EfCore/Helpers/BaseEfCoreRepositoryHelper.cs
@@ -0,0 +1,31 @@
+﻿using MultiShop.Identity.Infrastructure.Persistence.Exceptions;
+
+namespace MultiShop.Identity.Infrastructure.Persistence.EfCore.Helpers
+{
+    public static class BaseEfCoreRepositoryHelper
+    {
+        public static void ExecuteWithExceptionHandling(Action action,BaseEntityOperationFailedException exception)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception)
+            {
+                throw exception;
+            }
+        }
+
+        public static async Task ExecuteWithExceptionHandlingAsync(Func<Task> action,BaseEntityOperationFailedException exception)
+        {
+            try
+            {
+                await action();
+            }
+            catch (Exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
