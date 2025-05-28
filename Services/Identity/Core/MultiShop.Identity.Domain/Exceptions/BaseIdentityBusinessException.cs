7f7c197ddf48cc6a8cd810a85571e7254e0e26f0
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-05-28 14:39:40 +0300

    Base identity business exception has been created for use to identity business rules

---
+++ Services/Identity/Core/MultiShop.Identity.Domain/Exceptions/BaseIdentityBusinessException.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace MultiShop.Identity.Domain.Exceptions
+{
+    public class BaseIdentityBusinessException:Exception
+    {
+        public BaseIdentityBusinessException()
+        {
+            
+        }
+
+        public BaseIdentityBusinessException(string message) : base(message)
+        {
+
+        }
+
+        public BaseIdentityBusinessException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
