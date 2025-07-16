aa1895126ebb68168d31cfe377e57132a7539070
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-07-16 23:13:06 +0300

    Register User command handler and response classes have been created

--- Services/Identity/Core/MultiShop.Identity.Application/Features/Auth/Commands/RegisterUser/RegisterUserCommandHandler.cs
+++ Services/Identity/Core/MultiShop.Identity.Application/Features/Auth/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -1,13 +1,68 @@
 ﻿using MediatR;
+using MultiShop.Identity.Application.Helpers.Hashing;
+using MultiShop.Identity.Application.Interfaces.Repositories.User;
+using MultiShop.Identity.Application.Interfaces.UnitOfWork;
+using MultiShop.Identity.Domain.Entities;
 
 
 namespace MultiShop.Identity.Application.Features.Auth.Commands.RegisterUser
 {
     public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, RegisterUserCommandResponse>
     {
-        public Task<RegisterUserCommandResponse> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        private IUserRepository _userRepository;
+        private IUnitOfWork _unitofWork;
+
+        public RegisterUserCommandHandler(IUserRepository userRepository, IUnitOfWork unitofWork)
         {
-            throw new NotImplementedException();
+            _userRepository = userRepository;
+            _unitofWork = unitofWork;
+        }
+
+        public async Task<RegisterUserCommandResponse> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            var userExists = await CheckUserExistsAsync(request.UserName, request.Email);
+            if (userExists)
+            {
+                return new RegisterUserCommandResponse
+                {
+                    IsSuccess = false
+                };
+            }
+
+            HashingHelper.CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            var newUser = new User()
+            {
+                PasswordHash = passwordHash,
+                UserName = request.UserName,
+                FirstName= request.FirstName,
+                LastName = request.LastName,
+                PasswordSalt = passwordSalt,
+                Status = true
+            };
+
+            newUser.Email = request.Email;
+            newUser.AuthenticatorType = Domain.Enums.AuthenticatorType.None;
+
+            await _unitofWork.ExecuteInTransactionAsync(async (ct) =>
+            {
+                await _userRepository.InsertAsync(newUser);
+            });
+
+            var isSuccess = newUser.Id != 0 || !String.IsNullOrEmpty(newUser.Id.ToString());
+
+            return new RegisterUserCommandResponse
+            {
+                IsSuccess = isSuccess
+            };
+        }
+
+
+        private async Task<bool> CheckUserExistsAsync(string userName, string email)
+        {
+           
+            var entity=await _userRepository.GetByFilterAsync(u => u.UserName == userName);
+            return entity != null;
         }
     }
 }
--- Services/Identity/Core/MultiShop.Identity.Application/Features/Auth/Commands/RegisterUser/RegisterUserCommandResponse.cs
+++ Services/Identity/Core/MultiShop.Identity.Application/Features/Auth/Commands/RegisterUser/RegisterUserCommandResponse.cs
@@ -1,8 +1,10 @@
 ﻿
+using MultiShop.Identity.Application.Common;
+
 namespace MultiShop.Identity.Application.Features.Auth.Commands.RegisterUser
 {
-    public class RegisterUserCommandResponse
+    public class RegisterUserCommandResponse:BaseResponse,IHandleUserNotFound
     {
-        public bool IsSuccess { get; set; }
+       
     }
 }
