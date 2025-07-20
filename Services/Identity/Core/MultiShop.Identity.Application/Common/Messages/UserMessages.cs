8a0ba3ac00c3bf7fd1fe2a3f497b491537559592
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-07-20 21:01:29 +0300

    User validation messages and regexs have been defined to two static classes

--- Services/Identity/Core/MultiShop.Identity.Application/Common/Messages/UserMessages.cs
+++ Services/Identity/Core/MultiShop.Identity.Application/Common/Messages/UserMessages.cs
@@ -1,23 +1,33 @@
-﻿
-
-namespace MultiShop.Identity.Application.Common.Messages
+﻿namespace MultiShop.Identity.Application.Common.Messages
 {
     public static class UserMessages
     {
         public const string USERNAME_ALREADY_EXISTS = "Username is already taken";
+        public const string USERNAME_CAN_NOT_EMPTY = "Username can not be empty";
+        public const string USERNAME_IS_REQUIRED = "Username is required";
+        public static string USERNAME_CHARACTER_LIMIT(int min, int max) => $"Username must be between {min} and {max} characters";
+
+        public const string USERNAME_CAN_ONLY = "Username can only contain letters, numbers, and underscores";
+        public const string USERNAME_LENGTH = "Username's length can not be less ";
+
+
+        public const string EMAIL_CAN_NOT_EMPTY = "Email can not be empty";
         public const string EMAIL_ALREADY_EXISTS = "Email is already taken";
 
-        public const string USERNAME_CAN_NOT_EMPTY = "Username can not be empty";
-        public static readonly string EMAIL_CAN_NOT_EMPTY = "Email can not be empty";
+        public const string FIRST_NAME_CAN_NOT_EMPTY = "First name can not be empty";
+        public const string LAST_NAME_CAN_NOT_EMPTY = "Last name can not be empty";       
+        public const string FIRST_NAME_LENGTH = "First name's length can not be less";
+        public const string LAST_NAME_LENGTH = "Last name's length can not be less";
+        public const string FIRST_NAME_CAN_ONLY = "First name can only contain letters and spaces";
+        public const string LAST_NAME_CAN_ONLY = "Last name can only contain letters and spaces";
+        public static string NAME_CHARACTER_LIMIT(string namePropertyName, int min, int max) => $"{namePropertyName} must be between {min} and {max} characters";
 
+        public const string PASSWORD_IS_REQUIRED = "Password is required";
+        public static string PASSWORD_MIN_CHARACTER_LIMIT(int min) => $"Password must be at least {min} characters";
+        public const string PASSWORD_CAN_ONLY = "Password must contain at least one uppercase letter, one lowercase letter, and one number";
 
-        public const string FIRST_NAME_CAN_NOT_EMPTY = "Firstname can not be empty";
-        public const string LAST_NAME_CAN_NOT_EMPTY = "Lastname can not be empty";
-
-        public const string USERNAME_LENGTH = "Username's length can not be less ";
-        public const string FIRSTNAME_LENGTH = "Firstname's length can not be less";
-        public const string LASTNAME_LENGTH = "Lastname's length can not be less";
-
+        public const string IP_ADDRESS_REQUIRED = "Ip address is required";
+        public const string INVALID_IP_ADDRESS = "Invalid Ip address format";
 
         public const string USER_VALIDATION_FAILED = "User validation failed";
 
---
+++ Services/Identity/Core/MultiShop.Identity.Application/Common/Regexs/UserPropertyRegexs.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Identity.Application.Common.Regexs
+{
+    public static class UserPropertyRegexs
+    {
+        public const string UserNameContainRegex= "^[a-zA-Z0-9_]+$";
+        public const string PasswordContainRegex = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)";
+        public const string FirstNameContainRegex = "^[a-zA-ZğüşıöçĞÜŞİÖÇ ]+$";
+        public const string LastNameContainRegex = "^[a-zA-ZğüşıöçĞÜŞİÖÇ ]+$";
+
+
+    }
+}
