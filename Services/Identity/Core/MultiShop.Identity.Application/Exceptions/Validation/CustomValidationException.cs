33f7e5b81faa55448f9ebe6bfb70941c9778f124
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-07-20 21:02:20 +0300

    Custom exception has been created for validation

---
+++ Services/Identity/Core/MultiShop.Identity.Application/Exceptions/Validation/CustomValidationException.cs
@@ -0,0 +1,15 @@
+﻿namespace MultiShop.Identity.Application.Exceptions.Validation
+{
+    public class CustomValidationException:Exception
+    {
+        public IReadOnlyCollection<ValidationError> validationErrors;
+
+        public CustomValidationException(IReadOnlyCollection<ValidationError> errors) :
+          base("Validation failed")
+        {
+            validationErrors = errors;
+        }
+    }
+
+    public record ValidationError(string errorMessage,string propertyName);
+}
