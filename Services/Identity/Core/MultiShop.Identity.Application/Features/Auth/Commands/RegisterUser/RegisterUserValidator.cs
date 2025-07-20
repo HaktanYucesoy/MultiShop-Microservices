5b5a621025688efe0d37aa84d15462a90063b9c6
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-07-20 21:03:34 +0300

    Validation rules have been start to define and rule defination has not been done yet, i will create other rules for when user register

---
+++ Services/Identity/Core/MultiShop.Identity.Application/Features/Auth/Commands/RegisterUser/RegisterUserValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MultiShop.Identity.Application.Common.Messages;
+using MultiShop.Identity.Application.Common.Regexs;
+
+
+namespace MultiShop.Identity.Application.Features.Auth.Commands.RegisterUser
+{
+    public class RegisterUserValidator:AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(u => u.UserName).NotEmpty()
+                .WithMessage(UserMessages.USERNAME_CAN_NOT_EMPTY)
+                .Length(3, 50).WithMessage(UserMessages.USERNAME_CHARACTER_LIMIT(3, 50))
+                .Matches(UserPropertyRegexs.UserNameContainRegex).WithMessage(UserMessages.USERNAME_CAN_ONLY);
+        }
+    }
+}
