600b730070381af8f6a946caff08bf9e3a30f13f
HaktanYucesoy <haktanyucesoy936@gmail.com>
2025-07-29 13:57:02 +0300

    Dependency has been edited by MediatR pipelines and also add to automap and fluent validation dependency

--- Services/Identity/Core/MultiShop.Identity.Application/ApplicationServiceRegistration.cs
+++ Services/Identity/Core/MultiShop.Identity.Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using MultiShop.Identity.Application.Behaviours.Exception;
+using MultiShop.Identity.Application.Behaviours.Transaction;
+using MultiShop.Identity.Application.Behaviours.Validation;
+using MultiShop.Identity.Application.Common.Strategies.Failure.Factories;
+using System.Reflection;
 
 namespace MultiShop.Identity.Application
 {
@@ -7,6 +14,19 @@ namespace MultiShop.Identity.Application
 
         public static ServiceCollection AddApplicationServices(ServiceCollection services)
         {
+            var assembly=Assembly.GetExecutingAssembly();
+
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(assembly);
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>),typeof(ExceptionHandlingBehaviour<,>));
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>),typeof(TransactionBehaviour<,>));
+                
+            });
+            services.AddScoped<IFailureResponseStrategyFactory, FailureResponseStrategyFactory>();
+            services.AddValidatorsFromAssembly(assembly);
+            services.AddAutoMapper(assembly);
             return services;
         }
     }
